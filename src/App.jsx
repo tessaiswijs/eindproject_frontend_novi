bb9530d81e937b99a24bc5e9b559d65bcb804398
tessaiswijs <tessawijsman@gmail.com>
2025-12-06 18:34:56 +0100

    werkende login pagina

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import MealPlanning from './pages/mealPlanning/mealPlanningPage';
 import Quiz from './pages/quiz/QuizPage';
 import Recipe from './pages/recipe/RecipePage';
 import RecipeOverview from './pages/recipeOverview/RecipeOverviewPage';
-import Profile from './pages/profile/Profile.jsx';
+// import Profile from './pages/profile/Profile.jsx';
 import SignIn from './pages/signIn/SignInPage';
 import SignUp from './pages/signUp/SignUpPage';
 import NotFound from './pages/notFound/NotFoundPage';
@@ -26,23 +26,20 @@ function App() {
             <div className="content">
                 <Routes>
                     <Route path="/" element={<Home />}/>
-                    <Route path="/profile" element={isAuth ? <Profile /> : <Navigate to="/login"/>}/>
+
                     <Route path="/signin" element={<SignIn />}/>
                     <Route path="/signup" element={<SignUp />}/>
-                    <Route path="/quiz" element={<Quiz />}/>
-                    {/*<Route path="/quiz" {isAuth ? <MealPlanning /> : <Navigate to= "/signin"/>}*/}
+
+                    <Route path="/quiz" element={isAuth ? <Quiz /> : <Navigate to= "/signin"/>}/>
+                    <Route path="/mealplanning" element={isAuth ? <MealPlanning /> : <Navigate to= "/signin"/>}/>
 
                     <Route path="/recipeoverview" element={<RecipeOverview />}/>
                     <Route path="/recipe/:id" element={<Recipe />}/>
-                    <Route path="/mealplanning" element={<MealPlanning />}/>
-                    {/*<Route path="/mealplanning" {isAuth ? <MealPlanning /> : <Navigate to= "/signin"/>}*/}
+
                     <Route path="*" element={<NotFound />}/>
                 </Routes>
             </div>
-            <FooterBar />
-
-
-
+            <FooterBar/>
         </>
     );
 }
