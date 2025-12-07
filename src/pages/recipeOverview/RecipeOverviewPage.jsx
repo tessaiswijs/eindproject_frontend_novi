127ecfb95ea9c9014b50fc3dae18149fcbfcca69
tessaiswijs <tessawijsman@gmail.com>
2025-12-07 21:11:45 +0100

    toevoeging eerste stijling recipe overview pagina

--- src/pages/recipeOverview/RecipeOverviewPage.jsx
+++ src/pages/recipeOverview/RecipeOverviewPage.jsx
@@ -1,3 +1,4 @@
+import './RecipeOverviewPage.css';
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import RecipeCard from "../../components/recipeCard/RecipeCard.jsx";
